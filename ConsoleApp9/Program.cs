60eb7e14a1c8d67b88eedf2ab04331d4e09c8a8f
user432 <user432@inbox.ru>
2022-09-20 22:29:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        public interface IClonable
+        {
+            public object DeepCopy()
+            {
+                object figure = null;
+                using (MemoryStream tempStream = new MemoryStream())
+                {
+                    BinaryFormatter binFormatter = new BinaryFormatter(null,
+                        new StreamingContext(StreamingContextStates.Clone));
+
+                    binFormatter.Serialize(tempStream, this);
+                    tempStream.Seek(0, SeekOrigin.Begin);
+
+                    figure = binFormatter.Deserialize(tempStream);
+                }
+                return figure;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
